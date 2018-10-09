af9748e93f285c1d8344b06a3f918d053fd92129
rroca <r.roca@alumnos.upm.es>
2018-10-09 21:46:55 +0200

    Creada clase Empleado

---
+++ src/main/java/es/upm/entidades/Empleado.java
@@ -0,0 +1,17 @@
+package es.upm.entidades;
+
+import java.time.LocalDateTime;
+
+public class Empleado {
+
+    private String id;
+
+    private String nombre;
+
+    private LocalDateTime fechaContratacion;
+
+    public Empleado() {
+
+    }
+
+}
