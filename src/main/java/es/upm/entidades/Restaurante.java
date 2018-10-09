7e0a866eb65cb701bf03f996bed35bf2dba7d60b
rroca <r.roca@alumnos.upm.es>
2018-10-09 21:40:46 +0200

    Añadida clase Restaurante

--- src/main/java/es/upm/entidades/Restaurante.java
+++ src/main/java/es/upm/entidades/Restaurante.java
@@ -1,4 +1,37 @@
 package es.upm.entidades;
 
 public class Restaurante {
+
+    private String id;
+
+    private String nombre;
+
+    private String direccion;
+
+    public Restaurante() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
