960fae564c7a9049ed22f0ccd9d98c1825fcf30f
rrocamar <r.roca@alumnos.upm.es>
2018-10-10 13:23:13 +0200

    Modificada clase Empleado

--- src/main/java/es/upm/entidades/Empleado.java
+++ src/main/java/es/upm/entidades/Empleado.java
@@ -14,4 +14,27 @@ public class Empleado {
 
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDateTime getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(LocalDateTime fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
 }
