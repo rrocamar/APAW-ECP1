51890719b6f5c9b4e00c52d7451032a08a4867c4
rroca <r.roca@alumnos.upm.es>
2018-10-14 18:28:56 +0200

    Añado clase HorasFormacion.

---
+++ src/main/java/es/upm/entidades/strategy/HorasFormacion.java
@@ -0,0 +1,23 @@
+package es.upm.entidades.strategy;
+
+import es.upm.entidades.Empleado;
+
+public class HorasFormacion {
+
+    private int horasBonificables;
+
+    private int horasAcumuladas;
+
+    private int horasUtilizadas;
+
+    private EstrategiaCalculoHoras estrategia;
+
+    public HorasFormacion(Empleado empleado) {
+        //Inicializa las horas de formacion disponibles para el empleado que se pasa por parametro
+
+    }
+
+    public int calculaHorasDisponibles() {
+
+    }
+}
