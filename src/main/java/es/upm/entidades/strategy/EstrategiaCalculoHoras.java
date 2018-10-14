a22f3f76ab0797d48e8c0e8798023b4fe007dedb
rroca <r.roca@alumnos.upm.es>
2018-10-14 18:49:29 +0200

    Añado clase EstrategiaEmpleadoEsIndiferente

--- src/main/java/es/upm/entidades/strategy/EstrategiaCalculoHoras.java
+++ src/main/java/es/upm/entidades/strategy/EstrategiaCalculoHoras.java
@@ -2,5 +2,11 @@ package es.upm.entidades.strategy;
 
 public abstract class EstrategiaCalculoHoras {
 
+    public static final int NUMERO_MAXIMO_HORAS_ACUMULABLES = 100;
+
+    public int getMaximoHorasAcumulables() {
+        return NUMERO_MAXIMO_HORAS_ACUMULABLES;
+    }
+
     public abstract int calculaHorasDisponibles(int horasAcumuladas, int horasBonificables, int horasUtilizadas);
 }
--- src/main/java/es/upm/entidades/strategy/EstrategiaEmpleadoMeEsIndiferente.java
+++ src/main/java/es/upm/entidades/strategy/EstrategiaEmpleadoMeEsIndiferente.java
@@ -1,13 +1,13 @@
 package es.upm.entidades.strategy;
 
-public class EstrategiaEmpleadoMeGusta extends EstrategiaCalculoHoras {
+public class EstrategiaEmpleadoMeEsIndiferente extends EstrategiaCalculoHoras {
 
     @Override
     public int calculaHorasDisponibles(int horasAcumuladas, int horasBonificables, int horasUtilizadas) {
+        if (horasAcumuladas > getMaximoHorasAcumulables())
+            horasAcumuladas = getMaximoHorasAcumulables();
         int horasDisponibles = horasAcumuladas - horasUtilizadas - horasBonificables / 2;
-        if (horasDisponibles > 20)
-            return horasDisponibles;
-        else
-            return 20;
+
+        return horasDisponibles;
     }
 }
